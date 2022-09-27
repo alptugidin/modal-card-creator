7e6a9c244327f45e5fdffa181a92924b1f6cbc0f
Alptuğ <alptugidin@gmail.com>
2022-09-27 05:15:12 +0300

    removed

--- redux/store.ts
+++ redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
 import apperanceReducer from '../features/appearanceSlice';
+import targetingReducer from '../features/targetingSlice';
 
 const store = configureStore({
   reducer: {
     appearance: apperanceReducer,
+    targeting: targetingReducer,
   },
 });
 
