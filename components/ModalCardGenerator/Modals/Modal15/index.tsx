54646ed4a681b3add45a84a29334b8764722bbfb
Alptuğ <alptugidin@gmail.com>
2022-09-29 17:59:44 +0300

    Modal15 first

---
+++ components/ModalCardGenerator/Modals/Modal15/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const Modal15 = () => {
+  const fn = () => {};
+  const emojis = [<p>😶</p>, <p>☹</p>, <p>&#128524;</p>, <p>🤗</p>, <p>😄</p>];
+  const [select, setSelect] = useState(2);
+  return (
+    <div className="w-[400px] h-[226px] bg-white rounded-xl relative flex flex-col py-10 px-5">
+      <div className="absolute right-3 top-3">
+        <img src="/cancel.svg" alt="cancel" />
+      </div>
+      <div className="basis-1/2 font-[Inter] text-3xl font-semibold text-center flex items-center justify-center">
+        <p>Let’s feedback</p>
+      </div>
+      <div className="basis-1/2 flex justify-evenly items-center gap-">
+        {emojis.map((emoji, i) => (
+          <button
+            type="button"
+            onClick={() => setSelect(i)}
+            key={i.toString()}
+          >
+            <div className={`text-[36px] w-[59px] h-[59px] rounded-full border  text-center ${select === i ? 'border-purple-500 outline outline-purple-100  outline-4' : 'border-gray-400'}`}>
+              {emoji}
+            </div>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Modal15;
---
+++ stories/Modal15.stories.tsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Provider } from 'react-redux';
+import Modal15 from '../components/ModalCardGenerator/Modals/Modal15';
+import store from '../redux/store';
+
+export default {
+  title: 'Modal15',
+  component: Modal15,
+  decorators: [
+    (Story) => (<Provider store={store}><Story /></Provider>),
+  ],
+  argTypes: {
+    color: {
+      options: ['#000', '#777777', '#F37C34', '#7D4AEA'],
+      control: 'radio',
+    },
+  },
+} as ComponentMeta<typeof Modal15>;
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const Template: ComponentStory<typeof Modal15> = (args:any) => <Modal15 {...args} />;
+
+export const Base = Template.bind({});
