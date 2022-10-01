92739814d245657c5a4205733015c9bf0091102e
Alptuğ <alptugidin@gmail.com>
2022-10-01 05:21:31 +0300

    rewritten

--- stories/Modal1.stories.tsx
+++ stories/Modal1.stories.tsx
@@ -1,26 +1,27 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import React from 'react';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Provider } from 'react-redux';
 import Modal1 from '../components/ModalCardGenerator/Modals/Modal1';
+import store from '../redux/store';
 import '../components/ModalCardGenerator/Modals/Modal1/index.module.scss';
 
 export default {
   title: 'Modal1',
   component: Modal1,
-  argTypes: {
-    color: {
-      options: ['black', 'darkgrey', 'orange', 'default'],
-      control: 'radio',
-    },
-  },
+  decorators: [
+    (Story) => (
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    ),
+  ],
 } as ComponentMeta<typeof Modal1>;
-
 // eslint-disable-next-line react/jsx-props-no-spreading
 const Template: ComponentStory<typeof Modal1> = (args:any) => <Modal1 {...args} />;
 
-export const Sm = Template.bind({});
-Sm.args = { size: 'sm' };
-export const Md = Template.bind({});
-Md.args = { size: 'md' };
-export const Lg = Template.bind({});
-Lg.args = { size: 'lg' };
+Template.parameters = {};
+
+export const Base = Template.bind({});
--- stories/Modal3.stories.tsx
+++ stories/Modal3.stories.tsx
@@ -1,26 +1,27 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import React from 'react';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Provider } from 'react-redux';
 import Modal3 from '../components/ModalCardGenerator/Modals/Modal3';
+import store from '../redux/store';
 import '../components/ModalCardGenerator/Modals/Modal3/index.module.scss';
 
 export default {
   title: 'Modal3',
   component: Modal3,
-  argTypes: {
-    color: {
-      options: ['black', 'darkgrey', 'orange', 'default'],
-      control: 'radio',
-    },
-  },
+  decorators: [
+    (Story) => (
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    ),
+  ],
 } as ComponentMeta<typeof Modal3>;
-
 // eslint-disable-next-line react/jsx-props-no-spreading
 const Template: ComponentStory<typeof Modal3> = (args:any) => <Modal3 {...args} />;
 
-export const Sm = Template.bind({});
-Sm.args = { size: 'sm' };
-export const Md = Template.bind({});
-Md.args = { size: 'md' };
-export const Lg = Template.bind({});
-Lg.args = { size: 'lg' };
+Template.parameters = {};
+
+export const Base = Template.bind({});
