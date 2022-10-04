bec3bba29f546122b18a3d49b9b406e96c49fa1f
Alptuğ <alptugidin@gmail.com>
2022-10-04 06:14:30 +0300

    first

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Poppins:wght@500;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
