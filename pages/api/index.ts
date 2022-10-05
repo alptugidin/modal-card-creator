e7dd1a4744e7550c2a59cfdc156d5229931bd4c2
Alptuğ <alptugidin@gmail.com>
2022-10-05 22:06:03 +0300

    removed

--- pages/api/index.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-type Data = {
-  foo: string
-}
-
-export default (req:NextApiRequest, res:NextApiResponse<Data>) => {
-  res.status(200).json({ foo: 'bar' });
-};
